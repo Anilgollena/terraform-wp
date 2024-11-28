c8592b7f832b450d9ba187747504e369fdea270b
Anilgollena <115261722+Anilgollena@users.noreply.github.com>
2024-11-28 16:00:12 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "ram-1" {
+resource "aws_instance" "anil" {
   ami           = "ami-0166fe664262f664c" # Replace with your AMI ID          
   instance_type = "t2.micro"
   key_name      = "anil"
